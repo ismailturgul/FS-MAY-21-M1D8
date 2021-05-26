13469dd3899f95f5eaecca185987856004937942
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-05-26 14:52:34 +0200

    ex4
    Added function to change the URL

--- homeworkD7.js
+++ homeworkD7.js
@@ -7,3 +7,8 @@ const changePcontent = function()
 {
     document.getElementsByTagName("div")[0].getElementsByTagName("p")[0].innerText = "Hi"
 }
+
+const changeUrls = function()
+{
+    document.getElementsByTagName("a")[0].innerHTML = "www.google.de"
+}
