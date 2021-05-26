f46295dd36360c72fb531a5de37aa532c8a70517
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-05-26 14:44:23 +0200

    ex3 finished
    Added function to change the content of p

--- homeworkD7.js
+++ homeworkD7.js
@@ -2,3 +2,8 @@ const changeTitle = function(newTitle)
 {
     document.title =newTitle;
 }
+
+const changePcontent = function()
+{
+    document.getElementsByTagName("div")[0].getElementsByTagName("p")[0].innerText = "Hi"
+}
