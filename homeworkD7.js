82b4c4ed8e1e1ba14c6798b8e594d371babd6ac7
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-05-26 16:21:56 +0200

    ex7
    function added to let the ul dissapear

--- homeworkD7.js
+++ homeworkD7.js
@@ -21,8 +21,18 @@ const addToTheSecond = function()
     let newItem = document.createElement("li")
     document.getElementById("secondList").append(newItem)
 }
+//ex6
 const addParagraph = function()
 {
     let newItem = document.createElement("p")
     document.getElementsByTagName("div").append(newItem)
+}
+//ex7
+const firstUlDisappear = function()
+{
+    for (let i=0; i < 3; i++)
+    {
+        let x = document.getElementById("firstList").children[i] 
+        x.style.display = "none"
+    }
 }
