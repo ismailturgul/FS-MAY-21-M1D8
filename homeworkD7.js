d55713eaafdcb677de11f9981426acd3d27fd81e
ismailturgul <84572159+ismailturgul@users.noreply.github.com>
2021-05-26 15:15:45 +0200

    ex4
    added function to add a new item to the second list.

--- homeworkD7.js
+++ homeworkD7.js
@@ -1,14 +1,23 @@
+//ex1
 const changeTitle = function(newTitle)
 {
     document.title =newTitle;
 }
 
+//ex3
 const changePcontent = function()
 {
     document.getElementsByTagName("div")[0].getElementsByTagName("p")[0].innerText = "Hi"
 }
 
+//ex4
 const changeUrls = function()
 {
     document.getElementsByTagName("a")[0].innerHTML = "www.google.de"
+}
+//ex5
+const addToTheSecond = function()
+{
+    let newItem = document.createElement("li")
+    document.getElementById("secondList").append(newItem)
 }
